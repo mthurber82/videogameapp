a5b8e50a4c62a602e875c5a5ffd3f687393e0417
mado <madelineadonley@gmail.com>
2021-10-05 07:03:12 -0600

    js pseudo code

---
+++ script.js
@@ -0,0 +1,15 @@
+// grab elements with variables
+// create function to get RAWG api
+// fetch rawg url 
+// then statement
+// create for loop to dynamically create elements
+// create div element
+// create text element
+// create image element?
+// set element content and text
+// add classes to connect with stylesheet of choice
+// dynamically create buttons for youtube element
+// set youtube attribute to ("target", "_blank")
+// append div element to document
+// append text element to div element 
+// append button element to div element 
