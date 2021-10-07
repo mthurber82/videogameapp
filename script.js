3b5ee904f5c2af0b791fdfda498d49560e1deb4e
mado <madelineadonley@gmail.com>
2021-10-06 21:05:37 -0600

    access api data

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 // grab elements with variables
+var search = document.getElementById('search');
+var searchParameters = document.getElementById('searchgames');
+var gameInput = document.getElementById('needID');
+var platform = document.getElementById('needID');
+var genre = document.getElementById('form-stacked-select');
+var key = "385f0044190e471aa3e65b5f36e4f71a";
+
 // create function to get RAWG api
 // fetch rawg url 
 // then statement
@@ -13,7 +20,28 @@
 // append div element to document
 // append text element to div element 
 // append button element to div element 
-fetch("https://api.rawg.io/api/games?search=vampire-the-masquerade-bloodlines-2&key=" + APIKey)
+
+function searchGames() {
+    var gameSearchURL = 'https://api.rawg.io/api/games?search='+game+'&key='+key;
+
+    fetch(gameSearchURL)
+    .then(function (response) {
+        
+        return response.json();
+      
+    })
+    .then(function (data) {
+        console.log(data.result[0].name)
+        return data;
+    })
+}
+
+search.addEventListener("click", function() {
+    var games = gameInput.value;
+    searchGames(games);
+})
+
+fetch("https://api.rawg.io/api/games?search=vampire-the-masquerade-bloodlines-2&key=" + key)
     .then(function (response) {
         
         
