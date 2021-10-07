c9d7b61682bf84d319452bf64ebde880d193ddb2
mado <madelineadonley@gmail.com>
2021-10-06 21:08:02 -0600

    access api data

--- script.js
+++ script.js
@@ -6,50 +6,38 @@ var platform = document.getElementById('needID');
 var genre = document.getElementById('form-stacked-select');
 var key = "385f0044190e471aa3e65b5f36e4f71a";
 
-// create function to get RAWG api
-// fetch rawg url 
-// then statement
-// create for loop to dynamically create elements
-// create div element
-// create text element
-// create image element?
-// set element content and text
-// add classes to connect with stylesheet of choice
-// dynamically create buttons for youtube element
-// set youtube attribute to ("target", "_blank")
-// append div element to document
-// append text element to div element 
-// append button element to div element 
 
+// function to create a list of games upon search request
 function searchGames() {
+    // url variable
     var gameSearchURL = 'https://api.rawg.io/api/games?search='+game+'&key='+key;
 
     fetch(gameSearchURL)
+    // return response and convert to json
     .then(function (response) {
-        
         return response.json();
-      
     })
+    //return json response as data
     .then(function (data) {
-        console.log(data.result[0].name)
+        //create for loop to increment through each returned search result
+        for(var i = 0; i < data.results.length; i++) {
+            console.log(data.results[i].name);
+            // to do 
+            // dynamically create elements here
+            // set card values to data results
+            // dynamically create buttons for youtube element
+            // set youtube attribute to ("target", "_blank")
+            // append elements to page
+        }
         return data;
     })
-}
+};
 
+// add event listener to main search button
 search.addEventListener("click", function() {
-    var games = gameInput.value;
-    searchGames(games);
-})
+    // create variable for user search input
+    var game = gameInput.value;
+    // return function with user search input
+    searchGames(game);
+});
 
-fetch("https://api.rawg.io/api/games?search=vampire-the-masquerade-bloodlines-2&key=" + key)
-    .then(function (response) {
-        
-        
-        return response.json();
-        
-      
-    })
-    .then(function (data) {
-        console.log(data)
-        return data;
-    })
